81a76182a0fdef029b6ecbda39518d99fed3de31
Roberto Kodehode <roberto.kodehode@gmail.com>
2023-08-14 13:44:16 +0200

    changed name on variables

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 // global variables
 const formEl = document.getElementById("my-form"); // Reference to the form element
-const formText = document.getElementById("title-input"); // Reference to the pizza name input element
-const formDescription = document.getElementById("description-input"); // Reference to the pizza description input element
-const myPizzaContainer = document.getElementById("remember-list"); // Reference to the wrapper element to display pizzas
-const localStorageKey = "pizzaStorage"; // Key for storing data in local storage
+const formText = document.getElementById("title-input"); // Reference to the task name input element
+const formDescription = document.getElementById("description-input"); // Reference to the task description input element
+const myPizzaContainer = document.getElementById("remember-list"); // Reference to the wrapper element to display tasks
+const localStorageKey = "taskStorage"; // Key for storing data in local storage
 
-let toDoArray = JSON.parse(localStorage.getItem(localStorageKey)) || []; // Array to store pizza data retrieved from local storage or empty array if no data exists
+let toDoArray = JSON.parse(localStorage.getItem(localStorageKey)) || []; // Array to store task data retrieved from local storage or empty array if no data exists
 
-// Loop through the toDoArray and create pizza elements on page load
-toDoArray.forEach((pizzaItem) =>
-  createPizzaArray(pizzaItem.name, pizzaItem.descText, pizzaItem.id)
+// Loop through the toDoArray and create task elements on page load
+toDoArray.forEach((taskItem) =>
+  createPizzaArray(taskItem.name, taskItem.descText, taskItem.id)
 );
 
-// Function to add a new pizza
+// Function to add a new task
 function addPizza(text, descText) {
-  const pizzaId = Date.now(); // Generate a unique pizza ID based on the current timestamp
+  const taskId = Date.now(); // Generate a unique task ID based on the current timestamp
 
-  const pizza = {
+  const task = {
     name: text,
     descText: descText,
-    id: pizzaId,
+    id: taskId,
   };
 
-  toDoArray.push(pizza); // Add the new tas to the toDoArray
+  toDoArray.push(task); // Add the new tas to the toDoArray
   updateLocalStorage(); // Update the local storage with the updated toDoArray data
 
-  createPizzaArray(text, descText, pizzaId); // Create a pizza element on the page
+  createPizzaArray(text, descText, taskId); // Create a task element on the page
 }
 
 // Function to task element on the page
-function createPizzaArray(text, descText, pizzaId) {
+function createPizzaArray(text, descText, taskId) {
   const wrapper = document.createElement("article"); // Create a wrapper div
   wrapper.classList.add("wrapper");
 
@@ -43,11 +43,11 @@ function createPizzaArray(text, descText, pizzaId) {
   button.textContent = "check_circle";
   button.classList.add("btn-icon", "material-symbols-outlined");
 
-  button.addEventListener("click", () => removePizza(wrapper, pizzaId));
+  button.addEventListener("click", () => removeTask(wrapper, taskId));
 
   titleContainer.append(taskTitle, button);
 
-  const paragraph = document.createElement("p"); // Create a p element for the pizza description
+  const paragraph = document.createElement("p"); // Create a p element for the task description
   paragraph.textContent = descText;
 
   wrapper.append(titleContainer, paragraph); // Append the elements to the wrapper
@@ -56,10 +56,10 @@ function createPizzaArray(text, descText, pizzaId) {
 }
 
 // Function to remove a task from the page and toDoArray
-function removePizza(element, pizzaId) {
-  toDoArray = toDoArray.filter((pizza) => pizza.id !== pizzaId); // Filter out the pizza with the given pizzaId from the toDoArray
+function removeTask(element, taskId) {
+  toDoArray = toDoArray.filter((task) => task.id !== taskId); // Filter out the task with the given taskId from the toDoArray
   updateLocalStorage(); // Update the local storage with the updated toDoArray data
-  element.remove(); // Remove the pizza element from the page
+  element.remove(); // Remove the task element from the page
 }
 
 // Function to handle form submission
@@ -67,9 +67,9 @@ function handleForm(event) {
   event.preventDefault(); // Prevent the default form submission behavior
   if (formText.value.length < 3 || formDescription.value.length < 3) return;
 
-  addPizza(formText.value, formDescription.value); // Add a new pizza with the given name and description
-  formText.value = ""; // Clear the pizza name input field
-  formDescription.value = ""; // Clear the pizza description input field
+  addPizza(formText.value, formDescription.value); // Add a new task with the given name and description
+  formText.value = ""; // Clear the task name input field
+  formDescription.value = ""; // Clear the task description input field
 }
 
 //
